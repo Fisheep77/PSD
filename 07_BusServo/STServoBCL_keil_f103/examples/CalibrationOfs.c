dca12fea46f0f72024bf7976a51098ea5d3aa121
Fisheep77 <2915277551@qq.com>
2025-07-19 17:07:42 +0800

    other

---
+++ 07_BusServo/STServoBCL_keil_f103/examples/CalibrationOfs.c
@@ -0,0 +1,21 @@
+/*
+中位校准例子
+*/
+
+#include "stm32f10x.h"
+#include "SCServo.h"
+#include "uart.h"
+#include "wiring.h"
+
+void setup()
+{
+    Uart_Init(115200);
+    delay(1000);
+}
+
+void loop()
+{
+    CalibrationOfs(1);
+
+    while (1);
+}
